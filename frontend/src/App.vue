7c404e69750baeaf3635f444e194cdb2c197e421
pwh-pwh <plmoknpwh@163.com>
2024-07-23 17:45:14 +0800

    base

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 
+import Chat from "./components/Chat.vue";
 </script>
 
 <template>
-  <div class="">
-    demo
+  <div class="border-round-3xl p-3 surface-card">
+      <Chat />
   </div>
 </template>
 
---
+++ frontend/src/components/Chat.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="p-3 flex justify-content-center">
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
