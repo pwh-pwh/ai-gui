e09ab37bc65295fbf367e244f9e8489b5f55d980
pwh-pwh <plmoknpwh@163.com>
2024-07-23 14:13:54 +0800

    init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="">
+    demo
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/main.ts
@@ -0,0 +1,14 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeflex/primeflex.css';
+import './style.css';
+
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+app.mount('#app')
