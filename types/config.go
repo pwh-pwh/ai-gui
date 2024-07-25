36a20469a1269dcc512d6cd8ebf571b2caca2ce3
pwh-pwh <plmoknpwh@163.com>
2024-07-25 23:01:36 +0800

    config type

---
+++ types/config.go
@@ -0,0 +1,11 @@
+package types
+
+type Config struct {
+	BotType string    `json:"botType"`
+	Gpt     GptConfig `json:"gpt"`
+}
+
+type GptConfig struct {
+	BaseUrl string `json:"baseUrl"`
+	ApiKey  string `json:"apiKey"`
+}
