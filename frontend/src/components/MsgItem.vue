e53aeb067385bc1f2cf98d7b4b702be12633a050
pwh-pwh <plmoknpwh@163.com>
2024-07-25 20:11:18 +0800

    点击头像复制文字

--- frontend/src/components/MsgItem.vue
+++ frontend/src/components/MsgItem.vue
@@ -28,7 +28,7 @@ const copyText = () => {
 <template>
   <div :class="['flex align-items-start mb-4 px-3', positionClass]">
     <div class="w-3rem h-3rem flex align-items-center justify-content-center p-1 border-circle mx-2">
-      <Avatar :image="avatarImg" shape="circle" size="large"/>
+      <Avatar :image="avatarImg" shape="circle" size="large" @click="copyText"/>
     </div>
     <div class="dybg border-round px-1 w-full">
       <p class="text-red-800 text-left pl-2 text-xl white-space-normal">
