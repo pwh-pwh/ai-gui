624fb7b1fda3e8ef552685a77579ade02e57eeea
pwh-pwh <plmoknpwh@163.com>
2024-07-26 16:50:55 +0800

    1.去除pi-copy icon

--- frontend/src/components/MsgItem.vue
+++ frontend/src/components/MsgItem.vue
@@ -31,13 +31,15 @@ const copyText = () => {
 <template>
   <div :class="['flex align-items-start mb-4 px-3', positionClass]">
     <div class="w-3rem h-3rem flex align-items-center justify-content-center p-1 border-circle mx-2">
-      <Avatar :image="avatarImg" shape="circle" size="large" @click="copyText"/>
+      <Avatar v-if="message.userType === 'assistant'" :image="avatarImg" shape="circle" size="large" @click="copyText" v-tooltip="'copy text'"/>
     </div>
     <div class="dybg border-round px-1 w-full">
       <p class="text-red-800 text-left pl-2 text-xl white-space-normal">
         {{ message.content }}
       </p>
-      <i @click="copyText" v-if="message.userType === 'assistant'" v-tooltip="'copy text'" class="pi pi-copy text-gray-800 fixed right-0 mr-5" style="font-size: 1rem"></i>
+<!--      <div class="flex justify-content-end align-items-center">
+        <i @click="copyText" v-if="message.userType === 'assistant'" v-tooltip="'copy text'" class="pi pi-copy text-gray-800 mr-2 mb-2" style="font-size: 1rem"></i>
+      </div>-->
     </div>
   </div>
 </template>
